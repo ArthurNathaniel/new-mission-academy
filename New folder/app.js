880639a0703ebb8159a8783b05a74fa327d89125
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2022-11-21 05:49:44 +0000

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,37 @@
+// Navbar Responsivenesss
+function myFunction() {
+  var navbar = document.getElementById("nav");
+  navbar.classList.toggle("show");
+}
+
+
+
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  
